7eca782ff5d15768dbdf4a06f971e5f28c49f789
Jakub Bartosiewicz <jakub.bartosiewicz10@gmail.com>
2025-11-07 23:51:35 +0100

    Drugi commit

--- program.cpp
+++ program.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
+int silnia(int n)
+{
+	int wynik = 1;
+	for(int i = 1; i <= n; i++)
+	{
+		wynik = wynik*i;
+	}
+	return wynik;
+}
+
 int main()
 {
 	int n;
 	cin >> n;
-	cout << n << endl;
+	cout << silnia(n) << endl;
 
 	return 0;
 }
